6f3b0dc04fcf7554b88774e05fe8f6efb0e518a7
Ing. Michal Ďuračík <duracik2@fri.uniza.sk>
2018-04-03 07:25:57 +0200

    Çvicenie 7A - trieda Upratovacka

---
+++ src/fri/worldOfFri/prostredie/Upratovacka.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fri.worldOfFri.prostredie;
+
+/**
+ *
+ * @author duracik2
+ */
+public class Upratovacka extends Npc {
+    
+    public Upratovacka(PoziciaVRozhovore zaciatocnaPozicia) {
+        super("upratovacka", zaciatocnaPozicia);
+    }
+
+    @Override
+    public void rozhovor() {
+        super.rozhovor();
+    }
+    
+    
+    
+}
